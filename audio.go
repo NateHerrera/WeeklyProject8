08cc513f1e1d95edc90246e82ea6eb550ca8102a
Nate <150736820+NateHerrera@users.noreply.github.com>
2024-10-22 16:59:39 -0500

    Update audio.go

--- audio.go
+++ audio.go
@@ -1,4 +1,22 @@
 package main
 
+import rl "github.com/gen2brain/raylib-go/raylib"
+
+// creating a struct to hold the audio
 type Audio struct {
+	BackgroundMusic rl.Music
+	ShootingSound rl.Sound
+	// MonkeyDefeat rl.Sound
+	PlayerPoint rl.Sound
+}
+
+// create a global instance to hold all sounds
+var gameAudio Audio
+// load in the audio
+func LoadAudio () {
+	gameAudio.BackgroundMusic = rl.LoadMusicStream("assets/retrobackgroundmusic.wav")
+	gameAudio.ShootingSound = rl.LoadSound("assets/pewpew.wav")
+	// going to add a monkey defeat sound later
+	// gameAudio.MonkeyDefeat = rl.LoadSound("assets/")
+	gameAudio.PlayerPoint = rl.LoadSound("assets/playercollect.wav")
 }
